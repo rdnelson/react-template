a33e79a2d081e8dca15401e6831519682cd5a7e1
Robert Nelson <robert@rnelson.ca>
2016-05-04 23:27:02 -0400

    There are now usable tests

---
+++ src/index.spec.js
@@ -0,0 +1,11 @@
+import { describe} from "mocha";
+import { expect } from "chai";
+
+describe('Sample unit test', function() {
+    it('Should run unit tests', function() {
+    });
+
+    it('2 plus 2 should equal 4', function() {
+        expect(2+2).to.equal(4);
+    });
+});
